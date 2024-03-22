e86344a8551e347acad71d8b902de32c1f52d8f3
CodewithChayon <chayonbiswas872@gmail.com>
2024-03-23 02:27:23 +0600

    First Push

---
+++ MIDPY.py
@@ -0,0 +1,20 @@
+# Comment = input("What to say?:")
+# print(Comment)
+
+# a = input("Enter your First name: ")
+# print(len(a))
+
+# str = "hI! $Iam the $ symbol"
+#print(str.count('$'))
+# print(str.replace("$","@"))
+# print(str.capitalize())
+# print(str.find('hI'))
+# print(str.endswith("k"))
+
+marks =[2,1,3]
+# print(marks[-4:-1])
+# print(marks.remove(1))
+print(marks.reverse())
+# print(marks[-4:-1])
+# print(marks[-4:-1])
+# print(marks[-4:-1])
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,3 @@
-f = open("demo.txt", "a")
-
-# f.write("Then I'll move to ML")
-
-# f.close()
+print(str.count('$'))
+# print(str.count('$'))
+# print(str.count('$'))
